b7d43a3d90a3f2de0b91f82f454764368113d302
Tzvi-H <16guitar@gmail.com>
2022-05-04 19:47:32 -0500

    completed 8.7

--- index.js
+++ index.js
@@ -108,6 +108,7 @@ const typeDefs = gql`
       published: Int!
       genres: [String]
     ): Book
+    editAuthor(name: String!, setBornTo: Int!): Author
   }
 `;
 
@@ -134,6 +135,14 @@ const resolvers = {
       const newBook = { ...args, id: uuid() };
       books = books.concat(newBook);
       return newBook;
+    },
+    editAuthor(root, args) {
+      const author = authors.find((a) => a.name === args.name);
+      if (!author) return null;
+
+      const newAuthor = { ...author, born: args.setBornTo };
+      authors = authors.map((a) => (a.name === args.name ? newAuthor : a));
+      return newAuthor;
     },
   },
 };
