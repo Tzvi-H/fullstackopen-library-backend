146382af178741bc0607dd45e67a4f3f9bfa9921
Tzvi-H <16guitar@gmail.com>
2022-05-04 19:23:58 -0500

    completed 8.1

--- index.js
+++ index.js
@@ -26,16 +26,6 @@ let authors = [
   },
 ];
 
-/*
- * Suomi:
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
- *
- * English:
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
- */
-
 let books = [
   {
     title: "Clean Code",
@@ -90,11 +80,16 @@ let books = [
 
 const typeDefs = gql`
   type Query {
+    bookCount: Int!
+    authorCount: Int!
   }
 `;
 
 const resolvers = {
-  Query: {},
+  Query: {
+    bookCount: () => books.length,
+    authorCount: () => authors.length,
+  },
 };
 
 const server = new ApolloServer({
